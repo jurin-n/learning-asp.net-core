8629f2576d7abe35dda58e6de33817b7ed344a55
jurin-n <jurin.nakano@gmail.com>
2020-09-05 13:47:02 +0900

    fix #5

---
+++ WebApp/データベース作成.sql
@@ -0,0 +1,15 @@
+﻿/* dev2データベース作成 */
+USE master;
+GO
+IF DB_ID (N'dev2') IS NOT NULL
+DROP DATABASE dev2;
+GO
+CREATE DATABASE dev2
+COLLATE Japanese_CS_AS
+WITH TRUSTWORTHY ON, DB_CHAINING ON;
+GO
+--Verifying collation and option settings.
+SELECT name, collation_name, is_trustworthy_on, is_db_chaining_on
+FROM sys.databases
+WHERE name = N'dev2';
+GO
